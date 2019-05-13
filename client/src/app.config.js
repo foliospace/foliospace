0e6bdaefe2566e75c061c4e1f322bde3f9a12724
andreadias <diasan@oregonstate.edu>
2019-05-12 22:04:47 -0700

    installed yarn in main folder

--- client/src/app.config.js
+++ client/src/app.config.js
@@ -2,5 +2,5 @@ export default {
   url: 'https://dev-613949.okta.com',
   issuer: 'https://dev-613949.okta.com/oauth2/default',
   redirect_uri: window.location.origin + '/implicit/callback',
-  client_id: '{clientId}'
+  client_id: '0oajlmbbzksJN8t1Z356'
 };
--- server/lib/oktaClient.js
+++ server/lib/oktaClient.js
@@ -1,8 +1,8 @@
 const okta = require('@okta/okta-sdk-nodejs');
 
 const client = new okta.Client({
-  orgUrl: '{yourOktaDomain}',
-  token: '{yourApiToken}'
+  orgUrl: 'https://dev-613949.okta.com',
+  token: '008A421hKQLXiXMkm2Uy81DE8LdMJe1W3-fGFcHzgi'
 });
 
 module.exports = client;
