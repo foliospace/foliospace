d0b4a02248a8df04720d9c47ada09e93bb75f8cc
“artliou” <arthur.liou@c3iot.ai>
2019-05-12 20:41:35 -0700

    Add Procfile, Update Package.json scripts

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(morgan('short'));
 app.use(express.json());
