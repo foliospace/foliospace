053312d85cde5af3e612aa05120dab321b190fa0
“artliou” <arthur.liou@c3iot.ai>
2019-06-08 09:16:47 -0700

    UI Update - Favicon, homepage photo, add routes in navbar to include routes for Dashboard, Projects, and Account

--- client/src/app.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  url: 'https://dev-613949.okta.com',
-  issuer: 'https://dev-613949.okta.com/oauth2/default',
-  redirect_uri: window.location.origin + '/implicit/callback',
-  client_id: '0oajlmbbzksJN8t1Z356'
-};
